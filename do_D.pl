998851ebd8f32ebf2f58ec81528dc095e51a3c01
matteo14c <36039934+matteo14c@users.noreply.github.com>
2019-10-31 13:24:20 +0100

    Add files via upload

---
+++ do_D.pl
@@ -0,0 +1,53 @@
+#ALL paf ffiles
+@files=<*.paf>;
+#fasta di tutti i genomi
+@genomes=<*.fna>M
+
+
+foreach $f (@files)
+{
+	$g1="";
+	$g2="";
+	foreach $gen (@gen)
+	{
+		next if $gen eq $g1;
+		$g1=$gen if $f=~/$gen/ && $g1 eq "";
+		$g2=$gen if $f=~/$gen/;
+	}
+	die("could not determine the name of the first genome\n") if $g1 eq "";
+	die("could not determine the name of the second genome\n") if $g2 eq "";
+	open(IN,$f);
+	@IDS=();
+	@LNS=();
+	$TOT=0;
+	while(<IN>)
+	{
+		($l,$id)=(split())[10,-1];
+		$id=(split(/\:/,$id))[-1];
+		$id=100-100*$id;
+		next if $l<=1000;
+		$TOT+=$l;
+		push(@IDS,$id);
+		push(@LNS,$l);		
+	}
+	$ID=0;
+	for ($i=0;$i<=$#IDS;$i++)
+	{
+		$ID+=$IDS[$i]*$LNS[$i]/$TOT;
+	}
+	$sim_Data{$g1}{$g2}=$ID;
+	$sim_Data{$g2}{$g1}=$ID;
+}
+@genomes=sort(keys %sim_Data);
+print " @genomes\n";
+	
+foreach $g1 (@genomes)
+{
+	print "$g1 ";
+	foreach $g2 (@genomes)
+	{
+		$id = $g1 eq $g2 ? 100 : $sim_Data{$g1}{$g2};
+		print "$id ";
+	}
+	print "\n";
+}
